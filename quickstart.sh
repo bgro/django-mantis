a0d8a34151173ec83bf98eba987873c31dda77fb
Bernd Grobauer <bernd.grobauer@siemens.com>
2013-11-13 21:40:27 +0100

    Adding quickstart scripts.

--- quickstart.sh
+++ quickstart.sh
@@ -1,5 +1,7 @@
 python manage.py syncdb --traceback --settings=mantis.settings.local
 python manage.py migrate dingos --traceback --settings=mantis.settings.local
 python manage.py migrate mantis_core --traceback --settings=mantis.settings.local
+python manage.py mantis_openioc_set_naming --settings=mantis.settings.local  --trace 
+python manage.py mantis_stix_set_naming --settings=mantis.settings.local  --trace 
 python manage.py runserver 8000 --traceback --settings=mantis.settings.local
 
