021386b0e9fa4ed8f604854d1658e9cfc75b3265
Bernd Grobauer <bernd.grobauer@siemens.com>
2014-02-26 12:30:14 +0100

    Adding comments and collectstatic command to upgrade scripts.

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,11 @@
+echo "Upgrading from v0.1.0 to v0.2.0"
+echo "==============================="
+echo ""
+echo "Adding tables for models that have been introduced in v0.2.0"
+echo "------------------------------------------------------------"
 python manage.py migrate dingos --traceback --settings=mantis.settings.local
-
+echo ""
+echo "Adding static files that have been introduced in v0.2.0"
+echo "------------------------------------------------------------"
+python manage.py collectstatic --settings=mantis.settings.local  --trace
 
