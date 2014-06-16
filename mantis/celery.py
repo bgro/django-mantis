47022c37949361f8b47ea1119aaa5515efd3fb02
Bernd Grobauer <bernd.grobauer@siemens.com>
2014-06-16 11:28:25 +0200

    adding naming for stix and openioc; experimenting with celery.

--- mantis/celery.py
+++ mantis/celery.py
@@ -7,7 +7,10 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mantis.settings.donotcheckmein')
+
+if not settings.configured:
+    settings.configure()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mantis.settings.local_psql')
 
 app = Celery('mantis')
 
