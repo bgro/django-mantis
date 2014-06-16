fab1cd75b9a0403318e4ee5708c518972faca52a
Bernd Grobauer <bernd.grobauer@siemens.com>
2014-06-16 09:35:55 +0200

    Moving template from dingos to mantis.

--- mantis/menus.py
+++ mantis/menus.py
@@ -1,63 +1,31 @@
 from menu import Menu, MenuItem
 from django.core.urlresolvers import reverse
 
-Menu.add_item( "mantis_main",
-               MenuItem("List, Filter & Search", "",
-                        weight = 50,
-                        children = (
-                            MenuItem("Info Object List (generic filter)", reverse("url.dingos.list.infoobject.generic"), weight = 40 ),
-                            MenuItem("Info Object List (filter by ID)", reverse("url.dingos.list.infoobject.by_id"), weight = 50 ),
-                            MenuItem("Fact Search (simple)", reverse("url.dingos.search.fact.simple"), weight = 40 ),
-                            MenuItem("Fact Search (unique)", reverse("url.dingos.search.fact.simple.unique"), weight = 40 ),
-                        ),
-
-                        )
-)
-
-
-
-Menu.add_item( "mantis_main",
-               MenuItem("Saved Filters/Searches", "",
-                        weight = 50,
-                        children = ()
-
-               )
-)
-
-
 def user_name(request):
-    if request.user.is_authenticated():
-        return request.user.username
-    else:
-        return "Not logged in"
-
-def login_name(request):
-    if request.user.is_authenticated():
-        return "Log out"
-    else:
-        return "Log in"
+    return "%s, %s" % (request.user.last_name, request.user.first_name)
 
 
+# User menu when logged in
 Menu.add_item( "mantis_main",
                MenuItem(user_name,
                         "",
-                        weight = 50,
-                        children = (MenuItem("Edit user config", reverse("url.dingos.admin.view.userprefs"), weight = 40 ),
-                                    MenuItem("Edit saved searches", reverse("url.dingos.admin.edit.savedsearches"), weight = 40 ),
-                                    MenuItem(login_name,
+                        weight = 1000,
+                        children = (MenuItem("Edit user config", reverse("url.dingos.admin.view.userprefs"), weight = 10 ),
+                                    MenuItem("Edit saved searches", reverse("url.dingos.admin.edit.savedsearches"), weight = 20 ),
+                                    MenuItem("Log out",
                                              reverse("admin:logout"),
-                                             weight = 40,
-                                             # Seems that the check functionality of simple menu
-                                             # is somehow broken.
-                                             #check = lambda request: request.user.is_authenticated())
-                                    )
-                        )
-
+                                             weight = 100
+                                         )
+                        ),
+                        check = lambda request: request.user.is_authenticated()
                )
 )
 
-
-
-
-
-
+# Login button if user is not authenticated
+Menu.add_item( "mantis_main",
+               MenuItem("Log In",
+                        reverse("url.mantis.startpage"),
+                        weight = 1000,
+                        check = lambda request: not request.user.is_authenticated()
+               )
+)
