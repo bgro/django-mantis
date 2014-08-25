a04f7dd29b53771795f1e775176cc0cc081e1d67
Stefan Berger <stefanberger@siemens.com>
2014-08-25 13:31:31 +0200

    Added new API resources

--- mantis/urls.py
+++ mantis/urls.py
@@ -19,11 +19,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 
-
 # API Stuff
 from tastypie.api import Api
 from mantis_api_siemens.datasources import IkarusResource, SCDResource, CISOAREResource, CISONICResource, \
-                NICResource, ActiveDirectoryResource, pDNSResource
+                NICResource, ActiveDirectoryResource, pDNSSiemensResource, pDNSCIRCLResource
 from mantis_api.datasources import VirustotalResource
 
 v1_api = Api(api_name='v1.0')
@@ -38,54 +37,63 @@ v1_api.register(CISOAREResource.CISOAREResource())
 v1_api.register(CISONICResource.CISONICResource())
 v1_api.register(NICResource.NICResource())
 v1_api.register(ActiveDirectoryResource.ActiveDirectoryResource())
-v1_api.register(pDNSResource.pDNSResource())
+v1_api.register(pDNSSiemensResource.pDNSSiemensResource())
+v1_api.register(pDNSCIRCLResource.pDNSCIRCLResource())
 
 
+urlpatterns = patterns(
+    '',
+
+    # Default entry point
+    url(r'^$', lambda x: HttpResponseRedirect('/mantis/dashboard'), name="url.mantis.startpage" ),
 
 
-urlpatterns = patterns('',
-                       # Grappeli documentation
-                       (r'^grappelli/', include('grappelli.urls')),
-
-                       # Admin documentation:
-                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-                       # Admin Interface
-                       url(r'^admin/', include(admin.site.urls)),
-
-                       # MANTIS Urls -- currently, we just take the stuff from DINGOS
-                       # but that is likely to change soon
-
-                       url(r'^mantis/', include('dingos.urls')),
-
-                       # specialized views for STIX/CybOX objects
-                       url(r'^mantis/', include('mantis_stix_importer.urls')),
-
-                       # Include the authoring apps and the authoring base (dingos_authoring)
-                       url(r'^mantis/Authoring/', include('dingos_authoring.urls')),
-                       url(r'^mantis/Authoring/', include('mantis_authoring.urls')),
+    # Grappeli documentation
+    (r'^grappelli/', include('grappelli.urls')),
 
 
-                       # Include some test views
-                       url(r'^mantis/Test/Messaging', MessagingTestView.as_view()),
-
-                       # An empty home view
-                       url(r'^mantis/?$', HomeView.as_view()),
-
-                       # Include the Siemens Dashboard app
-                       url(r'^mantis/dashboard/', include('mantis_dashboard.urls', namespace='mantis_dashboard')),
-
-                       # Uncomment below to include URLs of MITRE's Yeti PoC app
-                       #url(r'^taxii/', include('yeti.urls')),
-
-                       # Our API
-                       url(r'^api/', include(v1_api.urls)),
-
-                       # OAuth2 Provider URLs
-                       url(r'^oauth2/', include('provider.oauth2.urls', namespace = 'oauth2')),
+    # Admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
 
-                       ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Admin Interface
+    url(r'^admin/', include(admin.site.urls)),
+
+
+    # MANTIS Urls -- currently, we just take the stuff from DINGOS
+    # but that is likely to change soon
+    url(r'^mantis/', include('dingos.urls')),
+
+
+    # Specialized views for STIX/CybOX objects
+    url(r'^mantis/', include('mantis_stix_importer.urls')),
+
+
+    # Include the authoring app and the authoring base (dingos_authoring)
+    url(r'^mantis/Authoring/', include('dingos_authoring.urls')),
+    url(r'^mantis/Authoring/', include('mantis_authoring.urls')),
+
+
+    # Include the Siemens Dashboard app
+    url(r'^mantis/dashboard/', include('mantis_dashboard.urls', namespace='mantis_dashboard')),
+
+
+    # Our API
+    url(r'^api/', include(v1_api.urls)),
+
+
+    # OAuth2 Provider URLs
+    url(r'^oauth2/', include('provider.oauth2.urls', namespace = 'oauth2')),
+
+
+    # Uncomment below to include URLs of MITRE's Yeti PoC app
+    #url(r'^taxii/', include('yeti.urls')),
+
+
+    # Debug toolbar URL mapping
+    #url(r'^__debug__/', include(debug_toolbar.urls)),
+
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.USE_DEBUG_TOOLBAR:
