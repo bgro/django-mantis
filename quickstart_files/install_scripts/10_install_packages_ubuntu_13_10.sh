c345dd6bd24016c284dce847bfac45fcabdc281d
Bernd Grobauer <mantis.cert@siemens.com>
2014-10-10 11:20:18 -0700

    Adding cd to scripts.

--- quickstart_files/install_scripts/10_install_packages_ubuntu_13_10.sh
+++ quickstart_files/install_scripts/10_install_packages_ubuntu_13_10.sh
@@ -1,3 +1,4 @@
+cd /home/mantis/ti
 echo "Running apt-get"
 sudo apt-get install libxml2 libxml2-dev python-dev libxslt1-dev python-virtualenv python-pip postgresql postgresql-server-dev-9.1 rabbitmq-server libldap2-dev
 echo "Copying celery scripts"
