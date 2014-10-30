6316038f06ea056dd1cc1e950c9ac4f94cae71e0
Bernd Grobauer <bernd.grobauer@siemens.com>
2014-10-30 13:33:09 +0100

    Disabling sqlite usage.

--- mantis/settings/local.py
+++ mantis/settings/local.py
@@ -74,15 +74,15 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-
-
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': '/tmp/django-mantis_test.db',
-            'USER': '',
-            'PASSWORD': '',
-            'HOST': '',
-            'PORT': '',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', #'django.db.backends.mysql', #'django.db.backends.postgresql_psycopg2', # 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'django',                      # Or path to database file if using sqlite3.
+        'USER': 'mantis',                      # Not used with sqlite3.
+        'PASSWORD': 'mantis',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
+}
+
+
