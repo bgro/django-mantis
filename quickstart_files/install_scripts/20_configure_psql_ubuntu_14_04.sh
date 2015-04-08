058f1722b3ab2cbbd1c5f9bc72a83ba022ebbec4
Philipp <philipp@langonline.net>
2015-04-08 13:34:53 +0200

    Fixing Ubuntu 14.04 scripts.

--- quickstart_files/install_scripts/20_configure_psql_ubuntu_14_04.sh
+++ quickstart_files/install_scripts/20_configure_psql_ubuntu_14_04.sh
@@ -1,14 +1,14 @@
 echo "Starting server"
-/etc/init.d/postgresql start 
+sudo /etc/init.d/postgresql start 
 echo "Creating mantis user"
-psql --command "CREATE USER mantis WITH PASSWORD 'mantis';"
-#psql --command "ALTER USER mantis CREATEDB;"
-psql --command "ALTER USER mantis WITH SUPERUSER;"
+sudo -u postgres psql --command "CREATE USER mantis WITH PASSWORD 'mantis';"
+#sudo -u postgres psql --command "ALTER USER mantis CREATEDB;"
+sudo -u postgres psql --command "ALTER USER mantis WITH SUPERUSER;"
 echo "Create Database"
-psql --command "CREATE DATABASE django OWNER mantis ENCODING 'UTF-8';"
+sudo -u postgres psql --command "CREATE DATABASE django OWNER mantis ENCODING 'UTF-8';"
 echo "Create language"
-psql --command "CREATE LANGUAGE plpythonu;" django
+sudo -u postgres psql --command "CREATE LANGUAGE plpythonu;" django
 echo "Modifying postgres configuration."
-echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/9.4/main/pg_hba.conf
+echo "host all  all    0.0.0.0/0  md5" | sudo tee -a /etc/postgresql/9.4/main/pg_hba.conf
 
 
